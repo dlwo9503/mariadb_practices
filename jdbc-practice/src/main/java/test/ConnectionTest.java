d6a84f774456d452b75a35c4a8109a36ade55088
dlwo9503 <dlwo9503@naver.com>
2021-05-20 10:05:21 +0900

    jdbc connection test

--- jdbc-practice/src/main/java/test/ConnectionTest.java
+++ jdbc-practice/src/main/java/test/ConnectionTest.java
@@ -7,20 +7,29 @@ import java.sql.SQLException;
 public class ConnectionTest {
 
 	public static void main(String[] args) {
+		Connection conn = null;
 		try {
 			// 1. JDBC Driver 로딩
 			Class.forName("org.mariadb.jdbc.Driver");
-			
+
 			// 2.
 			String url = "jdbc:mysql://192.168.254.31:3307/webdb";
-			Connection conn = DriverManager.getConnection(url, "webdb", "webdb"); // url, 이름, 비번
-			
+			conn = DriverManager.getConnection(url, "webdb", "webdb"); // url, 아이디, 비번
+
 			// 3. 연결 성공
 			System.out.println("ok : " + conn);
 		} catch (ClassNotFoundException e) {
 			System.out.println("드라이버 로딩 실패 : " + e);
 		} catch (SQLException e) {
 			System.out.println("error : " + e);
+		} finally {
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
