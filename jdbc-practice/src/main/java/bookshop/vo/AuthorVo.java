4829b40166a244f5a6e250982b5c1b5e28da4abd
dlwo9503 <dlwo9503@naver.com>
2021-05-20 16:06:47 +0900

    DAO 패턴 연습#2 - BookShop

---
+++ jdbc-practice/src/main/java/bookshop/vo/AuthorVo.java
@@ -0,0 +1,18 @@
+package bookshop.vo;
+
+public class AuthorVo {
+	private Long no;
+	private String name;
+	public Long getNo() {
+		return no;
+	}
+	public void setNo(Long no2) {
+		this.no = no2;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
