7afbc1064846b6ce2ac9be7a006c21b8a198e2c9
dlwo9503 <dlwo9503@naver.com>
2021-05-20 10:21:32 +0900

    .

--- jdbc-practice/src/main/java/driver/MyDriverTest.java
+++ jdbc-practice/src/main/java/driver/MyDriverTest.java
@@ -1,22 +1,22 @@
-package test;
+package driver;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConnectionTest {
+public class MyDriverTest {
 
 	public static void main(String[] args) {
 		Connection conn = null;
 		try {
 			// 1. JDBC Driver 로딩
-			Class.forName("org.mariadb.jdbc.Driver");
-
+			Class.forName("driver.MyDriver");
+			
 			// 2. 연결하기
-			String url = "jdbc:mysql://192.168.254.31:3307/webdb";
+			String url = "jdbc:mydb://127.0.0.1:2202/webdb";
 			conn = DriverManager.getConnection(url, "webdb", "webdb"); // url, 아이디, 비번
-
-			// 3. 연결 성공
+			
+			// 3.연결성공
 			System.out.println("ok : " + conn);
 		} catch (ClassNotFoundException e) {
 			System.out.println("드라이버 로딩 실패 : " + e);
